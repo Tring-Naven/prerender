559d104d9c169a33e616470e943690a1a77bebad
Tring-Naven <naven.a@tringapps.com>
2025-05-06 17:17:50 +0530

    build changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-           hello world test
+           hello world test v3 test
           </li>
         </ol>
 
